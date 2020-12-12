8ea7f6a778f126d6623023a9bd388c23bd9bbb76
greg <gsc229@gmail.com>
2020-12-12 03:00:16 -0500

    Exercise schema

---
+++ models/Exercise.js
@@ -0,0 +1,56 @@
+const { ObjectID } = require('bson')
+const mongoose = require('mongoose')
+
+const Exercise_Schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Must provide a name for the exercise"],
+    maxlength: [50, "Cannot exceed 50 characters"],
+    minlength: [3, "Cannot be less than 3 characters"]
+  },
+  category: {
+    type: String, 
+    enum: ["Endurance", "Strength", "Flexibility", "Balance"]
+  },
+  description: {
+    type: String,
+    maxlength: [50, "Cannot exceed 50 characters"],
+    minlength: [3, "Cannot be less than 3 characters"]
+  },
+  user: {
+    type: mongoose.Types.ObjectId,  
+    required: true, 
+    ref: 'User'
+  },
+  bodyPart: {
+    type: String, 
+    required: [true, "Must provide a body part"], 
+    maxlength: [50, "Cannot exceed 50 characters"],
+    minlength: [3, "Cannot be less than 3 characters"]
+  },
+  muscle_group: {
+    type: String,
+    maxlength: [50, "Cannot exceed 50 characters"],
+    minlength: [3, "Cannot be less than 3 characters"]
+  },
+  target_muscle: {
+    type: String,
+    maxlength: [50, "Cannot exceed 50 characters"],
+    minlength: [3, "Cannot be less than 3 characters"]
+  },
+  equipment: {
+    type: String,
+    default: 'none',
+    maxlength: [50, "Cannot exceed 50 characters"],
+    minlength: [3, "Cannot be less than 3 characters"]
+  },
+  video_url: {
+    type: String,
+    minlength
+  }
+
+})
+
+
+
+module.exports = mongoose.model('Exercise', Exercise_Schema)
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
@@ -14,11 +13,32 @@ dotenv.config({path: './config/config.env'})
 // Connect to database
 connectDB()
 
-// Route files
+// Logging middleware
+if(process.env.NODE_ENV==='development'){
+  app.use(morgan('dev'))
+}
 
-// start the app
+// Bring the route files to pass the entry point routes of each resource
+
+// Initiate the app
 const app = express()
+
+// Body parser
+app.use(express.json())
+
+// Set security headers
+app.use(helmet())
+
+// Enable CORS
+app.use(cors())
+
+// Mount the routers
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`\n *** LISTENING ON PORT ${PORT} ***\n`.yellow.bold))
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (error, promise) => {
+  console.log(`Error: ${error.message}`.red);
+});
